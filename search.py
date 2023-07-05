d87610745aa218e687bd79ce0da211bb6ab6f55b
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-07-05 11:47:27 +0800

    Update search.py

--- search.py
+++ search.py
@@ -7,11 +7,28 @@ from datetime import datetime
 import socket
 from serpapi import GoogleSearch
 
+
+#### part 0. main page setting
+st.set_page_config(page_title='ggsearch-Jiani', page_icon=':robot:')
+st.header("Ask Google")
+
+
+#### part 1. Instruction (sidebar)
+st.sidebar.title("Instruction")
+counter_placeholder = st.sidebar.empty()
+st.sidebar.info('''
+    You will be asked to have a conversation with ChatGPT to **generate a recipe**. \n
+    Following the chat, you’ll be redirected back to the survey to answer a few final questions and receive your payment code. 
+    \n Please paste down your participation ID and press Enter to submit: 
+    ''')
+user_id = st.sidebar.text_input("Participation ID...")   # ask for participation id
+
+
 #### Test. Present HTML (reference: https://github.com/serpapi/google-search-results-python)
 Google_API_KEY = st.secrets['Google_API_KEY']
 
-query = st.text_input("Participation ID...")   # ask for participation id
 
+query = st.text_input("Search")   # ask for participation id
 params = {
     "q": query,
     "device": "desktop",
@@ -26,3 +43,8 @@ search = GoogleSearch(params)
 out_html = search.get_html()
 # present
 components.html(out_html)
+
+
+#else:
+#    st.markdown("\n")
+#    st.markdown("Please read instructions in the sidebar carefully and type in your participant ID first!")
