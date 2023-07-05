ee5584777021f46ee3c4a3e41ed6d1ea3be6ae9a
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-07-05 20:38:50 +0800

    Update search.py

--- search.py
+++ search.py
@@ -29,6 +29,7 @@ Google_API_KEY = st.secrets['Google_API_KEY']
 
 
 st.markdown('<h1 style="background-color: gainsboro; padding-left: 10px; padding-bottom: 20px;">Google Search</h1>', unsafe_allow_html=True)
+st.markdown('\n')
 
 col1, col2 = st.columns(2)
 with col1:
