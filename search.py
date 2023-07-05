70658c7c15e3b139668f9d6bc7c1ffd9d2c08584
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-07-05 21:12:46 +0800

    Update search.py

--- search.py
+++ search.py
@@ -57,7 +57,7 @@ if query: #Activates the code below on hitting Enter/Return in the search textbo
         # In a few cases few individual search results doesn't have a description. In such cases the description would be blank
         description = "" if individual_search_result['snippet'] is None else individual_search_result['snippet']
         # Appending the result data frame after processing each individual search result
-        result_df = result_df.append(pd.DataFrame({"Title": url_txt, "URL": href, "Description": description}, index=[n]))
+        #result_df = result_df.append(pd.DataFrame({"Title": url_txt, "URL": href, "Description": description}, index=[n]))
         count_str = f'<b style="font-size:20px;">Google Search returned {len(result_df)} results</b>'
         ########################################################
         ######### HTML code to display search results ##########
@@ -71,7 +71,7 @@ if query: #Activates the code below on hitting Enter/Return in the search textbo
     st.markdown(f'{count_str}', unsafe_allow_html=True)
     st.markdown(f'{result_str}', unsafe_allow_html=True)
     st.markdown('<h3>Data Frame of the above search result</h3>', unsafe_allow_html=True)
-    st.dataframe(result_df)
+    #st.dataframe(result_df)
 
 # else:
 #    st.markdown("\n")
