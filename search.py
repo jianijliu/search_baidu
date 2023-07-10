fffd794cfa3b6e4be44e9e9901dc34f1581130ea
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-07-10 18:55:51 +0800

    Update search.py

--- search.py
+++ search.py
@@ -56,6 +56,15 @@ Google_API_KEY = st.secrets['Google_API_KEY']
 
 
 
+content = """<p><a href="https://github.com/vivien000/st-click-detector" id='Link 1'>First link</a></p>
+    <p><a href='#' id='Link 2'>Second link</a></p>
+    <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
+    <a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
+    """
+clicked = click_detector(content)
+
+st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
+
 
 
 if user_id: 
