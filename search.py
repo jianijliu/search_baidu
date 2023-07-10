8b91a9266e6f2fa764d8559264313c600359d4f2
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-07-10 17:09:12 +0800

    Update search.py

--- search.py
+++ search.py
@@ -89,7 +89,6 @@ if user_id:
             ########################################################
             if n < 10:
                 st.markdown( f'<tr style="border: none;"></tr>', unsafe_allow_html=True)
-                st.markdown( f'<tr style="border: none;"></tr>', unsafe_allow_html=True)
                 st.markdown( f'<tr style="border: none;">{url_displayed}</tr>', unsafe_allow_html=True)
                 st.markdown( f'<tr style="border: none;"><h4><a href="{href}" target="_blank">{url_txt}</a></h4></tr>', unsafe_allow_html=True)
                 st.markdown( f'<tr style="border: none;">{description}</tr>', unsafe_allow_html=True)
