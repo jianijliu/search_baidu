c7e8e58ce04d2340bbe5e447349b0c246bc3b116
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-07-10 18:17:11 +0800

    Update search.py

--- search.py
+++ search.py
@@ -59,7 +59,9 @@ content = """<p><a href='#' id='Link 1'>First link</a></p>
     <a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
     """
 clicked = click_detector(content)
-st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
+if clicked:
+    st.markdown(clicked)
+# st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
 
 if user_id: 
     # query = st.text_input(label=" ", placeholder="ask Lumina.AI")
