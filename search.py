6de8fc320b4badb5aff602db7446faedcb31fe36
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-07-11 20:55:11 +0800

    Update search.py

--- search.py
+++ search.py
@@ -15,13 +15,13 @@ from streamlit.components.v1 import html
 
 #### part 0. main page setting
 st.set_page_config(page_title='Lumina.AI', page_icon=':robot:')
-col1, col2, col3 = st.columns([1,6,1])
-with col1:
-    st.write("")
-with col2:
-    st.image("lumina.png", width=500)
-with col3:
-    st.write("")
+#col1, col2, col3 = st.columns([1,6,1])
+#with col1:
+#    st.write("")
+#with col2:
+#    st.image("lumina.png", width=500)
+#with col3:
+#    st.write("")
 # st.image(image='lumina.png', width=500)
 st.markdown('\n')
 
