d92c84216110489a252ff81e253a059462c04432
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-08-13 09:53:29 +0800

    Update search.py

--- search.py
+++ search.py
@@ -52,30 +52,26 @@ sheet_url = st.secrets["private_gsheets_url"]
 sheet = client.open_by_url(sheet_url).sheet1   # select a worksheet
 
 
-#### Set Google Search Key (reference: https://github.com/serpapi/google-search-results-python)
+# Set Google Search Key (reference: https://github.com/serpapi/google-search-results-python)
 Google_API_KEY = st.secrets['Google_API_KEY']
 
-def nav_to(url):
-    js = f'window.open("{url}", "_blank").then(r => window.parent.location.href);'
-    st_javascript(js)
-    
 if user_id: 
     # query = st.text_input(label=" ", placeholder="ask Lumina.AI")
     query = st.chat_input("ask Lumina.AI")
     
-    if query: #Activates the code below on hitting Enter/Return in the search textbox
+    if query:  # Activates the code below by hitting Enter/Return in the search textbox
         result_str = ""
         input_time = str(datetime.now())
         params = {"q": query, "device": "desktop", "hl": "en", "gl": "us", "num": "20", "api_key": Google_API_KEY, "output": "HTML"}
-        # define the search search
+        
+        # Define the search search
         search = GoogleSearch(params)
         json_results = search.get_json()
         search_result = json_results['organic_results']
-        # out_html = search.get_html()
-        # present
-        # components.html(out_html)
-    
-        result_df = pd.DataFrame() # Initializing the data frame that stores the results
+
+        st.write(search_result)
+        
+        # Initializing the data frame that stores the results
         result_str = ""
         save_str = ""
         for n, i in enumerate(search_result): #iterating through the search results
@@ -89,31 +85,8 @@ if user_id:
                 description = individual_search_result['snippet']
             else:
                 description = " "    
-                
-            # Present and Save the result data frame after processing each individual search result
-            # def click_button(href, st.session_state):
-            #    st.session_state[href] = True
-            #    webbrowser.open_new_tab(href)
-                # open_page(href)
-                # st.write(st.session_state)
-            #def click_button(href):
-            #    st.session_state[href] = True
-            #    nav_to(href)
-            #    # components.iframe(href)
-            #    # webbrowser.open_new_tab(href)
-            #    # st.write(f"Button {href} Clicked!")
-                
-            #hrefs = []
-            if n < 10:
-                #if href not in st.session_state:
-                #    st.session_state[href] = False   
-                #st.markdown('\n')
-                #st.write(url_displayed)
-                #st.button(url_txt, on_click=click_button, args=(hrefs))
-                #st.markdown(description)
-                #st.divider()
-                #hrefs.append(href)
 
+            if n < 10:
                 result_str += f'<tr style="border: none;"></tr>'+\
                 f'<tr style="border: none;"></tr>'+\
                 f'<tr style="border: none;">{url_displayed}</tr>'+\
@@ -126,23 +99,11 @@ if user_id:
                 save_str = "[" + str(n) + "] " + url_displayed + "|||||" + url_txt + "|||||" + href + "|||||" + description
                 row = [user_id, input_time, query, output_time, save_str]
                 sheet.insert_row(row)
-            else:
+            else:  # more than 10 results
                 pass
                 
         st.markdown(f'{result_str}', unsafe_allow_html=True)
 
-        #if click_detector(result_str):
-        #    st.write(result_str)
-        #    clicked = click_detector(result_str)
-        #    st.markdown(f"**{clicked} clicked**" if clicked != "" else "")
-        # st.write(st.session_state)
-        # for href in hrefs:
-        #    if st.session_state[href]:
-        #        st.write(st.session_state)
-                # webbrowser.open_new_tab(href)
-                # st.write(f"Button {href} Clicked!")
-        
 else:    
-    # st.header("")
     st.markdown("\n")
     st.markdown("<h2 style='text-align: center;'>Please read instructions in the sidebar carefully and \n type in your Prolific ID to initiate this service!</h2>", unsafe_allow_html=True)
