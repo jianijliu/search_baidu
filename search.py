6a6b8ed3e003f792bc2eab7dcb9a10d81c43944c
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-09-26 22:26:29 +0800

    Update search.py

--- search.py
+++ search.py
@@ -20,7 +20,7 @@ col1, col2, col3 = st.columns([1,6,1])
 with col1:
     st.write("")
 with col2:
-    st.image("logo-optima.png", width=500)   # lumina.png
+    st.image("logo-optima.png", width=500, height=300)   # lumina.png
 with col3:
     st.write("")
 st.markdown('\n')
