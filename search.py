4581d2886eee5e1c5e2515befbde4d7a034cb7fd
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-11-27 20:22:25 +0800

    Update search.py

--- search.py
+++ search.py
@@ -73,25 +73,19 @@ if user_id:
         # Initializing the data frame that stores the results
         result_str = ""
         save_str = " "
-        w = 0
         for n, i in enumerate(search_result): #iterating through the search results
-            st.write(url_txt, url_displayed, href)
             # Step1. read from retrieved results. 
             individual_search_result = i
             url_txt = individual_search_result['title'] #Finding the title of the individual search result
             url_displayed = individual_search_result['link']
             href = individual_search_result['link'] #title's URL of the individual search result
+            
             # (exception handle) In a few cases few individual search results doesn't have a description. In such cases the description would be blank
             if individual_search_result.get('snippet') != None:
                 description = individual_search_result['snippet']
             else:
                 description = " " 
-            x = 0  # indicate no google information; update for each result
-            for m in ['google', 'wikipedia']:
-                if x == 0:
-                    if m not in url_displayed:
-                        x = 1
-            if w < 10 and x == 1:
+            if n < 10:
                 result_str += f'<tr style="border: none;"></tr>'+\
                 f'<tr style="border: none;"></tr>'+\
                 f'<tr style="border: none;">{url_displayed}</tr>'+\
@@ -103,7 +97,6 @@ if user_id:
                 save_str += " [" + str(n) + "] " + url_displayed + "|||||" + url_txt + "|||||" + href + "|||||" + description
                 #row = [user_id, input_time, query, output_time, save_str]
                 #sheet.insert_row(row)
-                w += 1
             else:  # more than 10 results
                 pass
         row = [user_id, input_time, query, output_time, save_str]
